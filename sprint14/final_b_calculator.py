59b52c1aadb80dc89533742a3f4e9b6590791b03
ded <89196994468@yandex.ru>
2023-09-26 15:50:50 +0300

    sprint14_final_for_review

--- sprint14/final_b_calculator.py
+++ sprint14/final_b_calculator.py
@@ -1,59 +1,53 @@
-#  ID посылки xxx
+#  ID посылки 91409944
 
 class Stack:
-    def __init__(self, data):
-        self.stack = data
-        self.head = 0
-        self.length = 0
+    def __init__(self):
+        self.stack = []
 
-    def is_empty(self):
-        return self.length == 0
-    
-    def next(self):
-        self.head += 1
+    def add(self, x):
+        self.stack.append(int(x))
 
     def shift(self, res):
-        self.stack[self.head] = None
-        self.stack[self.head + 1] = None
-        self.stack[self.head + 2] = str(res)
-        self.head += 2
+        self.stack[-2] = res
+        self.stack.pop()
 
     def addition(self):
-        res = int(self.stack[self.head - 2]) + int(self.stack[self.head - 1])
+        res = self.stack[-2] + self.stack[-1]
         self.shift(res)
 
     def subtraction(self):
-        res = int(self.stack[self.head - 2]) - int(self.stack[self.head - 1])
+        res = self.stack[-2] - self.stack[-1]
         self.shift(res)
 
     def multiplication(self):
-        res = int(self.stack[self.head - 2]) * int(self.stack[self.head - 1])
+        res = self.stack[-2] * self.stack[-1]
         self.shift(res)
 
     def division(self):
-        res = int(self.stack[self.head - 2]) // int(self.stack[self.head - 1])
+        res = self.stack[-2] // self.stack[-1]
         self.shift(res)
 
 
 def input_data():
-    file_in = 'sprint14/input.txt'
-    with open(file_in, 'r') as f:
-        data = f.readline().strip().split()
-        # data = input().strip().split()
-    return data
+    return input().strip().split()
+
+
+def calculation(data):
+    stack = Stack()
+    for item in data:
+        if item.isdigit() or item[1:].isdigit():
+            stack.add(item)
+        else:
+            if item == '+':
+                stack.addition()
+            if item == '-':
+                stack.subtraction()
+            if item == '*':
+                stack.multiplication()
+            if item == '/':
+                stack.division()
+    return stack.stack[-1]
 
 
 if __name__ == '__main__':
-    data = input_data()
-    stack = Stack(data)
-    for item in stack.stack:
-        if item == '+':
-            stack.addition()
-        if item == '-':
-            stack.subtraction()
-        if item == '*':
-            stack.multiplication()
-        if item == '/':
-            stack.division()
-
-    print(stack.stack[stack.head])
+    print(calculation(input_data()))
