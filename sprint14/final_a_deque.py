8e35cf73a04249a6b3bc524b710e3a4a1418b191
ded <89196994468@yandex.ru>
2023-09-28 15:51:15 +0300

    sprunt14_final_for_review_2

--- sprint14/final_a_deque.py
+++ sprint14/final_a_deque.py
@@ -1,80 +1,78 @@
-#  ID посылки 91099563
+#  ID посылки 91582282
 
 class Deque:
     def __init__(self, max_size):
-        self.queue = [None] * max_size
-        self.max_n = max_size
-        self.head = 0
-        self.tail = 0
-        self.length = 0
+        self.__queue = [None] * max_size
+        self.__max_n = max_size
+        self.__head = 0
+        self.__tail = 0
+        self.__length = 0
 
-    def is_empty(self):
-        return self.length == 0
+    def __is_empty(self):
+        return self.__length == 0
+
+    def __is_full(self):
+        return self.__length == self.__max_n
+
+    def __index(self, command):
+        if command == 'push_back':
+            if self.__is_empty():
+                self.__head = 0
+                self.__tail = 0
+                return 0
+            return (self.__tail + 1) % self.__max_n
+        if command == 'push_front':
+            if self.__is_empty():
+                self.__head = 0
+                self.__tail = 0
+                return 0
+            return (self.__head - 1) % self.__max_n
+        if command == 'pop_front':
+            return (self.__head + 1) % self.__max_n
+        if command == 'pop_back':
+            return (self.__tail - 1) % self.__max_n
 
     def push_back(self, x):
-        if self.length != self.max_n:
-            self.queue[self.tail] = x
-            self.tail = (self.tail + 1) % self.max_n
-            self.length += 1
-        else:
-            return 'error'
+        if self.__is_full():
+            raise IndexError()
+        self.__tail = self.__index('push_back')
+        self.__queue[self.__tail] = x
+        self.__length += 1
 
     def push_front(self, x):
-        if self.length != self.max_n:
-            self.head = (self.head - 1) % self.max_n
-            self.queue[self.head] = x
-            self.length += 1
-        else:
-            return 'error'
+        if self.__is_full():
+            raise IndexError()
+        self.__head = self.__index('push_front')
+        self.__queue[self.__head] = x
+        self.__length += 1
 
     def pop_front(self):
-        if self.is_empty():
-            return 'error'
-        x = self.queue[self.head]
-        self.queue[self.head] = None
-        self.head = (self.head + 1) % self.max_n
-        self.length -= 1
+        if self.__is_empty():
+            raise IndexError()
+        x = self.__queue[self.__head]
+        self.__queue[self.__head] = None
+        self.__head = self.__index('pop_front')
+        self.__length -= 1
         return x
 
     def pop_back(self):
-        if self.is_empty():
-            return 'error'
-        x = self.queue[self.tail - 1]
-        self.queue[self.tail - 1] = None
-        self.tail = (self.tail - 1) % self.max_n
-        self.length -= 1
+        if self.__is_empty():
+            raise IndexError()
+        x = self.__queue[self.__tail]
+        self.__queue[self.__tail] = None
+        self.__tail = self.__index('pop_back')
+        self.__length -= 1
         return x
 
 
-def input_data():
-    commands = []
-    # count = int(input())
-    # deque_max = int(input())
-    # for _ in range(count):
-    #     commands.append(input().strip().split())
-    file_in = 'sprint14/input.txt'
-    with open(file_in, 'r') as f:
-        count = int(f.readline())
-        deque_max = int(f.readline())
-        for _ in range(count):
-            commands.append(f.readline().strip().split())
-    return deque_max, commands
-
-
 if __name__ == '__main__':
-    deque_max, commands = input_data()
-    stack = Deque(deque_max)
-    output = []
-    for command in commands:
-        if command[0] == 'push_back':
-            if stack.push_back(int(command[1])) == 'error':
-                output.append('error')
-        if command[0] == 'push_front':
-            if stack.push_front(int(command[1])) == 'error':
-                output.append('error')
-        if command[0] == 'pop_back':
-            output.append(stack.pop_back())
-        if command[0] == 'pop_front':
-            output.append(stack.pop_front())
-    for item in output:
-        print(item)
+    count = int(input())
+    deque = Deque(int(input()))
+    for _ in range(count):
+        try:
+            command, *args = input().strip().split()
+            output = getattr(deque, command)(*args)
+            if 'pop' in command:
+                print(output)
+        except IndexError:
+            print('error')
