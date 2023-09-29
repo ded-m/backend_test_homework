bd9108c147dfd2183029f18d94df41595e932360
ded <89196994468@yandex.ru>
2023-09-29 09:57:22 +0300

    sprint14_final_v3

--- sprint14/final_a_deque.py
+++ sprint14/final_a_deque.py
@@ -1,4 +1,4 @@
-#  ID посылки 91582282
+#  ID посылки 91639306
 
 class Deque:
     def __init__(self, max_size):
@@ -14,55 +14,45 @@ class Deque:
     def __is_full(self):
         return self.__length == self.__max_n
 
-    def __index(self, command):
-        if command == 'push_back':
+    def __index(self, index, command, direction):
+        if command == 'push':
             if self.__is_empty():
                 self.__head = 0
                 self.__tail = 0
                 return 0
-            return (self.__tail + 1) % self.__max_n
-        if command == 'push_front':
-            if self.__is_empty():
-                self.__head = 0
-                self.__tail = 0
-                return 0
-            return (self.__head - 1) % self.__max_n
-        if command == 'pop_front':
-            return (self.__head + 1) % self.__max_n
-        if command == 'pop_back':
-            return (self.__tail - 1) % self.__max_n
+        return (index + direction) % self.__max_n
 
-    def push_back(self, x):
+    def push_back(self, item):
         if self.__is_full():
             raise IndexError()
-        self.__tail = self.__index('push_back')
-        self.__queue[self.__tail] = x
+        self.__tail = self.__index(self.__tail, 'push', 1)
+        self.__queue[self.__tail] = item
         self.__length += 1
 
-    def push_front(self, x):
+    def push_front(self, item):
         if self.__is_full():
             raise IndexError()
-        self.__head = self.__index('push_front')
-        self.__queue[self.__head] = x
+        self.__head = self.__index(self.__head, 'push', -1)
+        self.__queue[self.__head] = item
         self.__length += 1
 
     def pop_front(self):
         if self.__is_empty():
             raise IndexError()
-        x = self.__queue[self.__head]
+        item = self.__queue[self.__head]
         self.__queue[self.__head] = None
-        self.__head = self.__index('pop_front')
+        self.__head = self.__index(self.__head, 'pop', 1)
         self.__length -= 1
-        return x
+        return item
 
     def pop_back(self):
         if self.__is_empty():
             raise IndexError()
-        x = self.__queue[self.__tail]
+        item = self.__queue[self.__tail]
         self.__queue[self.__tail] = None
-        self.__tail = self.__index('pop_back')
+        self.__tail = self.__index(self.__tail, 'pop', -1)
         self.__length -= 1
-        return x
+        return item
 
 
 if __name__ == '__main__':
@@ -72,7 +62,7 @@ if __name__ == '__main__':
         try:
             command, *args = input().strip().split()
             output = getattr(deque, command)(*args)
-            if 'pop' in command:
+            if not args:
                 print(output)
         except IndexError:
             print('error')
